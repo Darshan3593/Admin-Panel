bd1aaf639289106195b6f1a508d29fa0f74c62d2
Darshan3593 <61372457+Darshan3593@users.noreply.github.com>
2022-01-27 14:23:41 +0530

    final commit

---
+++ lib/config/config.dart
@@ -0,0 +1,9 @@
+
+class Config{
+  final String appName = 'Travellgo '; //app name
+  final String testerPassword = 'tester12345';  //testing password
+
+  //firebase server token for push notication
+  final String serverToken = 'AAAAaRDSzO4:APA91bEkofd-sP31hoLf9HpuzOPctWwHI-XMxCkc6tHNQ9k66MyOiHzSuEmct0FsKycsOTojYuwXM_FHhgrItjkHRRSRa11UeUg1bOvA31xa2amckJrzK4etos-SXZ0Fwx6FKQLb9mhj';
+  final String icon = 'assets/images/icon.png'; // app icon
+}
