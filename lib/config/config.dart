a65d5038397f79687ca033b8cb2f454224543daf
Darshan3593 <61372457+Darshan3593@users.noreply.github.com>
2022-02-19 13:05:16 +0530

    Testing commit

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -5,5 +5,5 @@ class Config{
 
   //firebase server token for push notication
   final String serverToken = 'AAAAaRDSzO4:APA91bEkofd-sP31hoLf9HpuzOPctWwHI-XMxCkc6tHNQ9k66MyOiHzSuEmct0FsKycsOTojYuwXM_FHhgrItjkHRRSRa11UeUg1bOvA31xa2amckJrzK4etos-SXZ0Fwx6FKQLb9mhj';
-  final String icon = 'assets/images/icon.png'; // app icon
+  final String icon = 'assets/images/icon.jpg'; // app icon
 }
